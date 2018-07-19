31564d8135c4ff653d7d7b37479770a676a59687
Pat <hang.patrick92@gmail.com>
2018-07-18 21:50:50 -0400

    Initial project with Odds

---
+++ WhatAreTheOdds/OddsLibrary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatAreTheOdds
+{
+    public class OddsLibrary
+    {
+        public OddsResults calculateOdds(int userValue, int opponentValue, int odds)
+        {
+            if (userValue == opponentValue)
+                return OddsResults.Match;
+            else if (userValue + opponentValue == odds)
+                return OddsResults.ReMatch;
+            else if (odds == 2)
+                return OddsResults.RoleReverse;
+            else
+                return OddsResults.NonMatch;
+        }
+
+        public bool ValidateInput(int userValue, int opponentValue, int odds)
+        {
+            return (userValue <= odds) && (opponentValue <= odds) && (userValue > 0) && (opponentValue > 0);
+        }
+    }
+
+    public enum OddsResults
+    {
+        NonMatch,
+        Match,
+        ReMatch,
+        RoleReverse
+    }
+}
