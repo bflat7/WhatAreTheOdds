8c28e1aaad2be0565047dd9a7d67ddad7c0cc619
Pat <hang.patrick92@gmail.com>
2018-07-18 22:03:31 -0400

    Pushing initial tests

---
+++ WhatAreTheOddsTest/OddsLibraryTests.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhatAreTheOdds;
+
+namespace WhatAreTheOddsTests
+{
+    [TestClass]
+    public class OddsLibraryTests
+    {
+        private static OddsLibrary oddsLibrary;
+
+        [ClassInitialize()]
+        public static void Initialize(TestContext testContext)
+        {
+            oddsLibrary = new OddsLibrary();
+        }
+
+        [TestMethod]
+        public void MatchTest()
+        {
+            OddsResults expectedValue = OddsResults.Match;
+            int userValue = 10;
+            int oppValue = 10;
+            int odds = 20;
+            OddsResults actualvalue = oddsLibrary.calculateOdds(userValue, oppValue, odds);
+            Assert.AreEqual(expectedValue, actualvalue);
+        }
+
+
+        [TestMethod]
+        public void NonMatchTest()
+        {
+            OddsResults expectedValue = OddsResults.NonMatch;
+            int userValue = 10;
+            int oppValue = 11;
+            int odds = 20;
+            OddsResults actualvalue = oddsLibrary.calculateOdds(userValue, oppValue, odds);
+            Assert.AreEqual(expectedValue, actualvalue);
+        }
+
+        [TestMethod]
+        public void ReMatchTest()
+        {
+            OddsResults expectedValue = OddsResults.ReMatch;
+            int userValue = 5;
+            int oppValue = 15;
+            int odds = 20;
+            OddsResults actualvalue = oddsLibrary.calculateOdds(userValue, oppValue, odds);
+            Assert.AreEqual(expectedValue, actualvalue);
+        }
+
+        [TestMethod]
+        public void RoleReversalTest()
+        {
+            OddsResults expectedValue = OddsResults.RoleReverse;
+            int userValue = 2;
+            int oppValue = 1;
+            int odds = 2;
+            OddsResults actualvalue = oddsLibrary.calculateOdds(userValue, oppValue, odds);
+            Assert.AreEqual(expectedValue, actualvalue);
+        }
+
+        [TestMethod]
+        public void BothInputInvalidTest()
+        {
+            bool expectedValue = false;
+            int userValue = 11;
+            int oppValue = 12;
+            int odds = 10;
+            bool actualValue = oddsLibrary.ValidateInput(userValue, oppValue, odds);
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void InvalidInputTest()
+        {
+            bool expectedValue = false;
+            int userValue = 9;
+            int oppValue = 12;
+            int odds = 10;
+            bool actualValue = oddsLibrary.ValidateInput(userValue, oppValue, odds);
+            Assert.AreEqual(expectedValue, actualValue);
+
+            userValue = 123;
+            oppValue = 9;
+            odds = 10;
+            actualValue = oddsLibrary.ValidateInput(userValue, oppValue, odds);
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void ValidInputTest()
+        {
+            bool expectedValue = true;
+            int userValue = 9;
+            int oppValue = 8;
+            int odds = 10;
+            bool actualValue = oddsLibrary.ValidateInput(userValue, oppValue, odds);
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void NegativeInputTest()
+        {
+            bool expectedValue = false;
+            int userValue = -9;
+            int oppValue = 8;
+            int odds = 10;
+            bool actualValue = oddsLibrary.ValidateInput(userValue, oppValue, odds);
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+    }
+}
---
+++ WhatAreTheOddsTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WhatAreTheOddsTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WhatAreTheOddsTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8b7d42d7-1f8c-4d7b-8013-441933a16265")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
